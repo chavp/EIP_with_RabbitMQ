aa31e22c22a32975d5cac03ba309adb5fdf0fb34
chavp <my.parinya@gmail.com>
2015-06-10 17:41:48 +0700

    Message Channel > Channel Adapter
    Signed-off-by: chavp <my.parinya@gmail.com>

---
+++ tutorials/TutorialsConsole/MathTool/MathChannelAdapter.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathTool
+{
+    using Math.Lib;
+    using MathRpc.Messages;
+    using Newtonsoft.Json;
+    using RabbitMQ.Client;
+    using RabbitMQ.Client.Events;
+
+    public class MathChannelAdapter
+        : IMath
+    {
+
+        private ConnectionFactory factory;
+        //private IModel channel;
+        //private string replyQueueName;
+       // private QueueingBasicConsumer consumer;
+
+        public MathChannelAdapter()
+        {
+            factory = new ConnectionFactory() { HostName = "localhost" };
+        }
+
+        public int Fib(int n)
+        {
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                var replyQueueName = channel.QueueDeclare();
+                var consumer = new QueueingBasicConsumer(channel);
+                channel.BasicConsume(replyQueueName, true, consumer);
+                var corrId = Guid.NewGuid().ToString();
+                var props = channel.CreateBasicProperties();
+                props.ReplyTo = replyQueueName;
+                props.CorrelationId = corrId;
+                props.ContentType = "application/json";
+
+                var message = JsonConvert.SerializeObject(new MathCommand { Math = "fib", N = n });
+
+                var messageBytes = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish("", "math_rpc_queue", props, messageBytes);
+
+                var ea = (BasicDeliverEventArgs)consumer.Queue.Dequeue();
+                if (ea.BasicProperties.CorrelationId == corrId)
+                {
+                    var result = Encoding.UTF8.GetString(ea.Body);
+                    var response = JsonConvert.DeserializeObject<Result>(result);
+                    return response.N;
+                }
+
+                return 0;
+            }
+        }
+    }
+}
---
+++ tutorials/TutorialsConsole/MathTool/MathViewModel.cs
@@ -0,0 +1,74 @@
+﻿using Math.Lib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MathTool
+{
+    public class MathViewModel : INotifyPropertyChanged
+    {
+        public int Number { get; set; }
+
+        int _Result = 0;
+        public int Result 
+        {
+            get
+            {
+                return _Result;
+            }
+            set
+            {
+                _Result = value;
+                RaisePropertyChanged("Result");
+            } 
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string propertyName)
+        {
+            // take a copy to prevent thread issues
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private ICommand _fibButtonCommand;
+        public ICommand FibCommand
+        {
+            get
+            {
+                return _fibButtonCommand;
+            }
+            set
+            {
+                _fibButtonCommand = value;
+            }
+        }
+
+        public void Fib(object obj)
+        {
+            if (this.Number > 40)
+            {
+                this.Result = 999999999;
+                return;
+            }
+            //++this.Result;
+            this.Result = _imath.Fib(this.Number);
+        }
+
+        private IMath _imath = null;
+
+        public MathViewModel()
+        {
+            _fibButtonCommand = new RelayCommand(Fib);
+            _imath = new MathChannelAdapter();
+        }
+    }
+}
